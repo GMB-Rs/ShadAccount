c1e9133d50b39bf97514a5041f4f107d492d1c95
Gabriel Martins Barbosa <gabrielmbarbosa00@gmail.com>
2025-08-04 01:30:24 -0300

    new

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: "/ShadAccount/",
+  base: "/shadbook-login/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
